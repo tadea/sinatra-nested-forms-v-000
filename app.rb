ae0fe9e31ba14a6b67dc04fda99829e195ab16bf
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-09-09 21:39:15 +0000

    automatically backed up by learn

---
+++ app.rb
@@ -0,0 +1,27 @@
+require './environment'
+
+module FormsLab
+  class App < Sinatra::Base
+
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+    erb :'pirates/new'
+  end
+
+  post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+
+    @ships = Ship.all
+
+    erb :'pirates/show'
+   end
+
+  end
+endß
